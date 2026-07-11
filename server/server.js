fc41d6f2a7137bdaa3fac529b3a0993251a786ce
Fathima <fathimae2000@gmail.com>
2026-07-11 12:16:28 +0530

    Fix duplicate cors require

--- server/server.js
+++ server/server.js
@@ -7,13 +7,17 @@ const errorHandler = require('./middlewares/errorHandler');
 
 require("./config/db");
 
-const cors=require('cors')
-
-
-
 app.use(express.json());
 
-app.use(cors())
+const cors = require('cors');
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',  
+    'https://small-erp.vercel.app'  
+  ],
+  credentials: true
+}));
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/ProductRoutes');
@@ -24,11 +28,10 @@ const inventoryRoutes = require('./routes/inventoryRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const chatRoutes = require("./routes/chatRoutes");
-const searchRoutes=require('./routes/Dashboardsearchroutes')
-
+const searchRoutes = require('./routes/Dashboardsearchroutes')
 
 app.use('/api/auth', userRoutes);
-app.use('/api/products',productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/sales', saleRoutes);
 app.use('/api/purchases', purchaseRoutes);
 app.use('/api/customers', customerRoutes);
@@ -38,11 +41,7 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/message", chatRoutes);
 app.use("/api/dashboard/search", searchRoutes);
 
-
-
-
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
